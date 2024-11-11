086667bd12cf0c0615851cc09ea7f78e6eda0308
Vartem4got <artwitch3000@gmail.com>
2024-11-11 10:09:44 +0200

    Update Ship_Class.py
    Renamed pasngrs into passengers_count for easier code reading

--- src/Ship_Class.py
+++ src/Ship_Class.py
@@ -1,10 +1,10 @@
 class Ship:
 
-    def __init__(self, name="Unknown", mass=0, pasngrs=0):
+    def __init__(self, name="Unknown", mass=0, passengers_count=0):
 
         self.__name = name             
         self.__mass = mass       
-        self.__pasngrs = pasngrs
+        self.__passengers_count = passengers_count
         self.public_int = 0            
         self.public_str = "Public"     
 
@@ -15,8 +15,8 @@ class Ship:
     def get_mass(self):
         return self.__mass
 
-    def get_pasngrs(self):
-        return self.__pasngrs
+    def get_passengers_count(self):
+        return self.__passengers_count
 
     # Def set ->
     def set_name(self, name):
@@ -25,16 +25,16 @@ class Ship:
     def set_mass(self, mass):
         self.__mass = mass
 
-    def set_pasngrs(self, pasngrs):
-        self.__pasngrs = pasngrs
+    def set_passengers_count(self, pasngrs):
+        self.__passengers_count = passengers_count
 
     # str repl del ->
     def __str__(self):
-        return f"Ship(Name: {self.__name}, Mass: {self.__mass}, Pasngrs: {self.__pasngrs})"
+        return f"Ship(Name: {self.__name}, Mass: {self.__mass}, Pasngrs Count: {self.__passengers_count})"
 
 
     def __repr__(self):
-        return f"Ship('{self.__name}', {self.__mass}, {self.__pasngrs})"
+        return f"Ship('{self.__name}', {self.__mass}, {self.__passengers_count})"
 
 
     def __del__(self):
@@ -47,18 +47,11 @@ def main():
     ship2 = Ship("Queen Mary", 76000, 2700)
     ship3 = Ship()
 
-
+    
     print(ship1)
     print(ship2)
     print(ship3)
 
-    
-        # i hate debug, if it goes wrong agan i dknow what to do
-    
-
-    print(ship3)
-    
-    
     
 
 if __name__ == "__main__":
